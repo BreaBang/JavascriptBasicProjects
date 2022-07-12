e96ac8603ca8fa80dc9fdf037017a2d582c59885
Breanna Bang <101009515+BreaBang@users.noreply.github.com>
2022-07-12 12:52:51 -0600

    Add files via upload

---
+++ Modal/app.js
@@ -0,0 +1,14 @@
+// select modal-btn,modal-overlay,close-btn
+
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay")
+const modalClose = document.querySelector(".close-btn")
+//listen for click events on modal-btn and close-btn
+modalBtn.addEventListener("click", function (){
+// when user clicks modal-btn add .open-modal to modal-overlay
+    modalOverlay.classList.add("open-modal");
+});
+// when user clicks close-btn remove .open-modal from modal-overlay
+modalClose.addEventListener('click', function(){
+    modalOverlay.classList.remove("open-modal")
+})
